d0d0fadd910744ed82772d75c0ed6d0e73b1ad87
Guilherme <128387905+GuiTails@users.noreply.github.com>
2023-04-17 18:03:08 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+print("Digite o número que quer testa")
+N = int(input("> "))
+i = 1
+while True:
+    if N <=0:
+        print("Número inválido")
+    if N ==1:
+        print("Primo")
+        break
+    i = i + 1
+    if N % i ==0 and N !=i:
+        print("Não primo")
+        print(i,"é seu divisor")
+        break
+    if N % i !=0 and i > 10000000000000000000000000000000000:
+        if i > 10000000000000000000000000000000000:
+            print("Número muito grande")
+            break
+        if i < 10000000000000000000000000000000000:
+            print("Primo")
+            break
